8158719d51405f837618396df3025bb4eeaf5589
igaeun <igaeun0112@gmail.com>
2023-11-10 04:34:28 +0900

    영화 상세 설명 수정

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -41,13 +41,28 @@ const MovieOverview = styled.div`
     top: 0;
     left: 0;
     width: 100%;
-    background-color: rgba(15, 0, 102, 0.658);
-    color: white;
-    font-size: 12px;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.658);
     border-bottom-left-radius: 15px;
     border-bottom-right-radius: 15px;
     padding: 10px;
     display: none;
+`
+
+const MovieOverviewTitle = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 20px;
+`
+
+const MovieOverviewContent = styled.div`
+  font-size: 12px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 15px;
+  line-height: 1.5;
 `
 
 const MovieContainerWrapper = styled.div`
@@ -71,7 +86,12 @@ export default function Movie ({ title, posterPath, voteAverage, overview }) {
         {/* 포스터 이미지 */}
         <MovieContainerImg src={IMG_BASE_URL + posterPath} alt='포스터'/>
         {/* 영화 상세 설명 */}
-        <MovieOverview>{overview}</MovieOverview>
+        <MovieOverview>
+          {/* 영화 상세 설명 > 제목 */}
+          <MovieOverviewTitle>{title}</MovieOverviewTitle>
+          {/* 영화 상세 설명 > 설명 */}
+          <MovieOverviewContent>{overview}</MovieOverviewContent>
+        </MovieOverview>
         <MovieInfo>
           {/* 영화 제목 */}
           <div id='movie-title'>{title}</div>
