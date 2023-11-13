825a9a19b6fae6d6bbd228245fa189eece29adda
igaeun <igaeun0112@gmail.com>
2023-11-13 14:41:50 +0900

    Home > AdPage, NotFound 라우팅

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import AdPage from '../components/AdPage'
+import NotFound from './NotFound'
 
 export default function Home () {
   return (
     <div>
-      <AdPage />
+      <Routes>
+        <Route path="/" element={<AdPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   )
 }
